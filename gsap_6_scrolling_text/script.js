4c82c607133ffe90835d5e1634aa3651c7df1344
MosfiqurRubel <rubel.airin@gmail.com>
2026-03-16 20:37:31 +0600

    Marque Animaiton

---
+++ gsap_6_scrolling_text/script.js
@@ -0,0 +1,28 @@
+const marqueAnimation = () => {
+  window.addEventListener("wheel", (dets) => {
+    if (dets.deltaY > 0) {
+      gsap.to(".marque", {
+        transform: "translateX(-100%)",
+        duration: 4,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to(".marque img", {
+        rotate: 180,
+      });
+    } else {
+      gsap.to(".marque", {
+        transform: "translateX(100%)",
+        duration: 4,
+        repeat: -1,
+        ease: "none",
+      });
+      gsap.to(".marque img", {
+        rotate: 0,
+      });
+    }
+  });
+};
+
+marqueAnimation();
